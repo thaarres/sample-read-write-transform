b3b58894874547b8692f73a31f34f47326e36809
Kinga Anna Wozniak <kingusiu@gmail.com>
2020-06-03 19:02:06 +0200

    adding option to limit number of events in event_to_image_serialization

--- event_to_image_serialization.py
+++ event_to_image_serialization.py
@@ -58,19 +58,29 @@ class ImageSerializer():
             f.create_dataset('eventFeatureNames',data=[l.encode('utf-8') for l in labels]) # encode python3 unicode for h5py
         print('wrote {0} event image pairs to {1}'.format(dijet_features.shape[0],out_path))
 
+    def shuffle_unisono(self, jet1_data, jet2_data, dijet_data):
+        assert len(jet1_data) == len(jet2_data) == len(dijet_data)
+        p = np.random.permutation(len(jet1_data))
+        return [jet1_data[p], jet2_data[p], dijet_data[p]]
 
-    def read_events_write_images(self, in_path, out_path ):
+    def read_events_write_images(self, in_path, out_path, n_evts ):
         ''' reads: jet-constituents (2 jets x n events x m particles x 3 features ), dijet-features and labels,
             writes: jet-images ( 2 jets x n events x n_bins x n_bins x 1 channel ), dijet-features (unmodified), lables (unmodified)
             with events cut at mjj_cut and images normalized by pt
         '''
         # read events
         events_j1, events_j2, dijet_features, labels = self.read_file( in_path )
-        print('read {} events'.format(dijet_features.shape[0]))
+        n_evts_read = dijet_features.shape[0]
+        print('read {} events'.format(n_evts_read))
+
+        # reduce to number of events given
+        if n_evts_read > n_evts:
+            events_j1, events_j2, dijet_features = self.shuffle_unisono(events_j1, events_j2, dijet_features)
+            events_j1, events_j2, dijet_features = events_j1[:n_evts], events_j2[:n_evts], dijet_features[:n_evts]
+
         # mass cut
         mjj_idx = labels.index('mJJ')
-        mjj_arr = dijet_features[:,mjj_idx]
-        events_j1, events_j2, dijet_features = ut.filter_arrays_on_value( events_j1, events_j2, dijet_features, filter_arr=mjj_arr, filter_val=self.mjj_cut )
+        events_j1, events_j2, dijet_features = ut.filter_arrays_on_value( events_j1, events_j2, dijet_features, filter_arr=dijet_features[:,mjj_idx], filter_val=self.mjj_cut )
         # convert to images
         images_j1, images_j2 = self.convert_events_to_image( events_j1, events_j2 )
         # normalize by pt
@@ -86,10 +96,11 @@ if __name__ == "__main__":
     parser.add_argument('-in', dest='infile', type=str, help='input file name/path')
     parser.add_argument('-out', dest='outfile', type=str, default='out.h5', help='output file name/path')
     parser.add_argument('-bin', dest='n_bins', type=int, default=32, help='number of bins in jet image')
+    parser.add_argument('-n', dest='num_evts', type=int, default=1e9, help='number of events for output dataset')
 
     args = parser.parse_args()
 
     print('converting data in file', args.infile)
 
     serializer = ImageSerializer( args.n_bins )
-    serializer.read_events_write_images( args.infile, args.outfile )
+    serializer.read_events_write_images( args.infile, args.outfile, args.num_evts )
