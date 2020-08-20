eee3605467cba022feb283e6ecbf311958f51b38
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-08-20 15:46:04 +0200

    adding N option to concat run script

--- run_evt_concat.sh
+++ run_evt_concat.sh
@@ -1,4 +1,4 @@
 export IN=/eos/project/d/dshep/TOPCLASS/DijetAnomaly/qcd_sqrtshatTeV_13TeV_PU40
 export OUT=/eos/user/k/kiwoznia/data/VAE_data/march_2020_data/input/events
-python3 -u event_concatenate_serialization.py -in /eos/project/d/dshep/TOPCLASS/DijetAnomaly/qcd_sqrtshatTeV_13TeV_PU40 -out /eos/user/k/kiwoznia/data/VAE_data/march_2020_data/input/events/qcd_sqrtshatTeV_13TeV_PU40_mjj_cut_concat.h5
+python3 -u event_concatenate_serialization.py -in /eos/project/d/dshep/TOPCLASS/DijetAnomaly/qcd_sqrtshatTeV_13TeV_PU40 -out /eos/user/k/kiwoznia/data/VAE_data/march_2020_data/input/events/qcd_sqrtshatTeV_13TeV_PU40_mjj_cut_concat_2M.h5 -n 2000000
 
