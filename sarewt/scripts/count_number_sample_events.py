20d6ec4441eb1e422fffbac94d3a5c95a08d009c
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-11-26 16:39:23 +0100

    adding new production path to event count script

--- sarewt/scripts/count_number_sample_events.py
+++ sarewt/scripts/count_number_sample_events.py
@@ -2,8 +2,8 @@ import sarewt.data_reader as dare
 import os
 import glob
 
-base_dir = '/eos/project/d/dshep/TOPCLASS/DijetAnomaly/VAE_results/run_101/sample_results/minl1l2_loss'
-sample_dirs =  glob.glob(base_dir+'/*q*/*')
+base_dir = '/eos/user/k/kiwoznia/data/VAE_data/events'
+sample_dirs =  glob.glob(base_dir+'/*')
 
 print('*'*10+'\ncounting number of events and files in subdirectories of {}\n'.format(base_dir)+'*'*10)
 
@@ -18,4 +18,4 @@ for sample_dir in sample_dirs:
 	num_events.append(n_events)
 
 for sample_dir, nn, ff in zip(sample_dirs, num_events, num_files):
-	print("{: <40}: {: >10} events in {: >10} files".format(os.path.basename(sample_dir), nn, ff))
+	print("{: <52}: {: >10} events in {: >10} files".format(os.path.basename(sample_dir), nn, ff))
